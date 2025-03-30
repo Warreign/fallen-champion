76547078fb558dd03f18bd636bf4ba0b805c2967
Dmytro Kadar <dmytrokadar@gmail.com>
2025-03-30 11:35:17 +0200

    audio manager

---
+++ Scripts/audio_manager.gd
@@ -0,0 +1,9 @@
+extends Node
+
+@onready var jump: AudioStreamPlayer = $Jump
+@onready var hit: AudioStreamPlayer = $Hit
+@onready var doping_taken: AudioStreamPlayer = $DopingTaken
+@onready var background_music: AudioStreamPlayer = $BackgroundMusic
+
+func _on_background_music_finished() -> void:
+	background_music.play()
--- Scripts/game_controller.gd
+++ Scripts/game_controller.gd
@@ -3,8 +3,8 @@ class_name GameController
 extends Node2D
 
 func generate_obstacles() -> void:
-    $Player.get_viewport_rect()
-    return
+	$Player.get_viewport_rect()
+	return
 
 func _process(delta: float) -> void:
-    pass
+	pass
